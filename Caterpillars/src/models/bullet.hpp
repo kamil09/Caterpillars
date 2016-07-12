f8d656e57921bc919e09f615c7cf2517625058a6
Kamil <kpiotrowski@comea.pl>
2016-07-12 19:39:48 +0200

    rysowanie pocieku

--- Caterpillars/src/models/bullet.hpp
+++ Caterpillars/src/models/bullet.hpp
@@ -23,8 +23,6 @@ public:
    void draw(glm::mat4 projection, glm::mat4 modelView, glm::mat4 lights, glm::vec3 sun);
    void setPos(float x,float y,float z);
    glm::vec3 startLook;
-   float viewBack;
-   float tmpViewBack;
    float Damage;
 };
 
